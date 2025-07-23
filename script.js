aece6d2d4ed71dd06267ccbc6b6ae2e629e80b9c
smileywrld <ismaileyyunusa@gmail.com>
2025-07-23 17:11:52 +0100

    implemented the regular expression for verifying the email

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// const express = require('express');
-// const app = express();
-
-// app.use(express.static('public', { type: 'module' }));
-
-const email = document.querySelector('.email');
-const errorElement = document.querySelector('.error');
+const email = document.querySelector(".email");
+const errorElement = document.querySelector(".error");
 const emailValue = email.value.trim();
 
-const button = document.getElementById('myButton');
-button.addEventListener('click', function (event) {
-  event.preventDefault();
-  validateMessage();
-  let userMail = localStorage.setItem('Email', email.value);
-  if (!errorElement.textContent) {
-    window.location.href = '/desktop-success.html';
-  }
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", function (event) {
+	event.preventDefault();
+ 
 });
+
+function validateMail() {
+
+  let testEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  if (testEmail.test(emailValue)) {
+   errorElement.innerHTML = ""
+    window.location.href = "/desktop-success.html";
+  }
+ 
+}
